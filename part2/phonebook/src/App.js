9ab71ffad4f178baa329ceb0472fae5e1dc48ab6
Andrey <konohov.andreyka@gmail.com>
2023-07-13 13:02:11 +0300

    done: 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ search, handleSearchChange }) => {
   return (
@@ -36,15 +37,22 @@ const Persons = (props) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    console.log('effect');
+
+    const eventHandler = (response) => {
+      console.log('promise fulfilled');
+      setPersons(response.data);
+    };
+
+    const promise = axios.get('http://localhost:3002/persons');
+    promise.then(eventHandler);
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
