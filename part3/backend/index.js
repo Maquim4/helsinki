f092db26d08aaa12e240cb44c0fd3f9f3ae73f64
Andrey <konohov.andreyka@gmail.com>
2023-07-28 12:01:32 +0300

    add: validation error handler

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -9,24 +9,6 @@ app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-];
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
@@ -61,9 +43,12 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -75,14 +60,14 @@ app.delete('/api/notes/:id', (request, response, next) => {
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
+  Note.findByIdAndUpdate(
+    request.params.id,
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then((updatedNote) => {
       response.json(updatedNote);
     })
@@ -101,6 +86,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
