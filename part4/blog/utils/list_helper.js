c614ff689e8f0945373747ad10bbf6a4bbffb52c
Andrey <konohov.andreyka@gmail.com>
2023-08-03 10:31:09 +0300

    test: create tests for blog logic

---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,44 @@
+const _ = require('lodash');
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, cur) => acc + cur.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const { _id, url, __v, ...clone } = blogs.find(
+    (blog) => blog.likes === Math.max(...blogs.map((blog) => blog.likes))
+  );
+  return clone;
+};
+
+const mostBlogs = (blogs) => {
+  let result = Object.entries(
+    blogs.reduce(
+      (acc, cur) => ((acc[cur.author] = ++acc[cur.author] || 1), acc),
+      {}
+    )
+  ).reduce((max, cur) => (max[1] > cur[1] ? max : cur));
+  return { author: result[0], blogs: result[1] };
+};
+
+const mostLikes = (blogs) => {
+  return Object.entries(_.groupBy(blogs, 'author'))
+    .map((arr) => _.concat(arr[0], [_.sumBy(arr[1], 'likes')]))
+    .reduce(
+      (acc, cur) =>
+        acc.likes > cur[1] ? acc : { author: cur[0], likes: cur[1] },
+      {}
+    );
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
