1e411b2b5d40291d0eab0e01c1804bd1e411a01c
Andrey <konohov.andreyka@gmail.com>
2023-08-10 18:05:03 +0300

    add: token-based authentication

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,9 +2,11 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
 blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+  Blog.find({})
+    .populate('user')
+    .then((blogs) => {
+      response.json(blogs);
+    });
 });
 
 blogsRouter.post('/', (request, response, next) => {
